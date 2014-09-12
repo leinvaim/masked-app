08f3cf2d703276cb871ce018fb8309ac461ba953
Craig McNamara <cmcnamara87@gmail.cok>
2014-09-13 02:59:34 +1000

    feat(explore): add explore cell and network

---
+++ masked/ExploreCollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  ExploreCollectionViewCell.h
+//  masked
+//
+//  Created by Craig McNamara on 13/09/2014.
+//  Copyright (c) 2014 leinvaim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ExploreCollectionViewCell : UICollectionViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *postImageView;
+@end
---
+++ masked/ExploreCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  ExploreCollectionViewCell.m
+//  masked
+//
+//  Created by Craig McNamara on 13/09/2014.
+//  Copyright (c) 2014 leinvaim. All rights reserved.
+//
+
+#import "ExploreCollectionViewCell.h"
+
+@implementation ExploreCollectionViewCell
+
+@end
